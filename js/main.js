c96feea83c1363cf1e4fb36a3021da340ddcf790
CircleCI <circleci@seehisface.com>
2020-01-17 20:32:45 +0000

    Trigger deployment 20:32 Jan 17, 2020

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function navtoggle() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("topnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
+} 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topnav").style.top = "0";
+  } else {
+    document.getElementById("topnav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 } 
