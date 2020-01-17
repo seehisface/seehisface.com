72102ea9494be72853f874b6229009a523c6ddd0
CircleCI <circleci@seehisface.com>
2020-01-17 17:50:41 +0000

    Trigger deployment 17:50 Jan 17, 2020

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function nav-tog() {
+function navtoggle() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
