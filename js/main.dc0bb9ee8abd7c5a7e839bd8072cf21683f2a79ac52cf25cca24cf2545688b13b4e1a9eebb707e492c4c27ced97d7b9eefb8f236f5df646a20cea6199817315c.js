1c1f2b99a023e55c12e7e32a67ebbdaee86f4605
CircleCI <circleci@seehisface.com>
2020-12-18 19:08:43 +0000

    Trigger deployment 19:08 Dec 18, 2020

--- js/main.dc0bb9ee8abd7c5a7e839bd8072cf21683f2a79ac52cf25cca24cf2545688b13b4e1a9eebb707e492c4c27ced97d7b9eefb8f236f5df646a20cea6199817315c.js
+++ js/main.dc0bb9ee8abd7c5a7e839bd8072cf21683f2a79ac52cf25cca24cf2545688b13b4e1a9eebb707e492c4c27ced97d7b9eefb8f236f5df646a20cea6199817315c.js
@@ -25,18 +25,3 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 } 
-
-//WebFontConfig = {
-    //google: { families: [ 'Roboto+Slab' ] }
-//};
-
-//(function() {
-    //var wf = document.createElement('script');
-    //wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    //wf.type = 'text/javascript';
-    //wf.async = 'true';
-    //var s = document.getElementsByTagName('script')[0];
-    //s.parentNode.insertBefore(wf, s);
-//})();
-
-
